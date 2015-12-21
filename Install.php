ba2b955ddb6d11dcc246857e9de6d5d8fd150a18
Pavel910 <pavel@webiny.com>
2015-12-21 21:03:04 +0100

    Install

--- Install.php
+++ Install.php
@@ -3,13 +3,12 @@
 namespace Webiny\Core;
 
 use Composer\Script\Event;
-use Composer\Installer\PackageEvent;
 
 class Install
 {
 
     public static function postInstall(Event $event)
     {
-        echo "POST INSTALL CALLED";
+        echo "\nWEBINY: POST INSTALL \n\n";
     }
 }
