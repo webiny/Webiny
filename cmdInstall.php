238494d279d1350430e9cb3858221f133cf7dfe3
Pavel910 <pavel@webiny.com>
2015-12-21 21:09:02 +0100

    Install

--- cmdInstall.php
+++ cmdInstall.php
@@ -1,4 +1,5 @@
 <?php
 
-echo "=========== WEBINY/CORE ===========\n";
-echo "Current DIR: " . realpath(__DIR__) . "\n\n";
+echo "\n=========== WEBINY/CORE ===========\n";
+echo "Current DIR: " . realpath(__DIR__) . "\n";
+echo "Working DIR: " . getcwd() . "\n\n";
