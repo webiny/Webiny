c3b0d04f880d4670d5214ccf646ed5d4980b4b97
Pavel910 <pavel@webiny.com>
2016-04-13 15:58:31 +0200

    styles vendor folder

---
+++ Js/Webiny/Assets/bower_components/filesize/Gruntfile.js
@@ -0,0 +1,88 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg : grunt.file.readJSON("package.json"),
+		concat: {
+			options : {
+				banner : "/**\n" + 
+				         " * <%= pkg.name %>\n" +
+				         " *\n" +
+				         " * @copyright <%= grunt.template.today('yyyy') %> <%= pkg.author %>\n" +
+				         " * @license <%= pkg.license %>\n" +
+				         " * @version <%= pkg.version %>\n" +
+				         " */\n"
+			},
+			dist: {
+				src : [
+					"<banner>",
+					"src/intro.js",
+					"src/filesize.js",
+					"src/outro.js"
+				],
+				dest : "lib/filesize.es6.js"
+			}
+		},
+		"babel": {
+			options: {
+				sourceMap: false,
+				presets: ["babel-preset-es2015"]
+			},
+			dist: {
+				files: {
+					"lib/<%= pkg.name %>.js": "lib/<%= pkg.name %>.es6.js"
+				}
+			}
+		},
+		eslint: {
+			target: ["lib/<%= pkg.name %>.es6.js"]
+		},
+		nodeunit : {
+			all : ["test/*.js"]
+		},
+		sed : {
+			"version" : {
+				pattern : "{{VERSION}}",
+				replacement : "<%= pkg.version %>",
+				path : ["<%= concat.dist.dest %>"]
+			}
+		},
+		uglify: {
+			options: {
+				banner : "/*\n" +
+				" <%= grunt.template.today('yyyy') %> <%= pkg.author.name %>\n" +
+				" @version <%= pkg.version %>\n" +
+				" */",
+				sourceMap: true,
+				sourceMapIncludeSources: true
+			},
+			target: {
+				files: {
+					"lib/filesize.min.js" : ["lib/filesize.js"]
+				}
+			}
+		},
+		watch : {
+			js : {
+				files : "<%= concat.dist.src %>",
+				tasks : "build"
+			},
+			pkg: {
+				files : "package.json",
+				tasks : "build"
+			}
+		}
+	});
+
+	// tasks
+	grunt.loadNpmTasks("grunt-sed");
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-nodeunit");
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-babel");
+	grunt.loadNpmTasks("grunt-eslint");
+
+	// aliases
+	grunt.registerTask("test", ["eslint", "nodeunit"]);
+	grunt.registerTask("build", ["concat", "sed", "babel"]);
+	grunt.registerTask("default", ["build", "test", "uglify"]);
+};
