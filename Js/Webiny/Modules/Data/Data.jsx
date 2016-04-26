fff6b64f151d162dee2f2b2d8c235fca618aeff7
Pavel910 <pavel@webiny.com>
2016-04-26 15:56:01 +0200

    Data module for easy fetching/filtering of data and forwarding it to desired components.

---
+++ Js/Webiny/Modules/Data/Data.jsx
@@ -0,0 +1,46 @@
+import Webiny from 'Webiny';
+
+class Data extends Webiny.Ui.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: null
+        };
+
+        this.bindMethods('setData,filter');
+        Webiny.Mixins.ApiComponent.extend(this);
+    }
+
+    componentWillMount() {
+        super.componentWillMount();
+        if (!_.isFunction(this.props.children)) {
+            console.warn('Warning: Data component only accepts a function as its child element!');
+        }
+    }
+
+    componentDidMount() {
+        super.componentDidMount();
+        this.api.execute().then(this.setData);
+    }
+
+    setData(apiResponse) {
+        if (apiResponse.isError()) {
+            return Webiny.Growl.info(apiResponse.getError(), 'Could not fetch data');
+        }
+        this.setState({data: apiResponse.getData()});
+    }
+
+    filter(filters = {}) {
+        this.api.setQuery(filters).execute().then(this.setData);
+    }
+}
+
+Data.defaultProps = {
+    renderer() {
+        return _.isFunction(this.props.children) ? this.props.children(this.state.data, this.filter) : null;
+    }
+};
+
+export default Data;
