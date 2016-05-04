d294af308777132f4a087f23a3748e9a4ff94c31
sven <sven@webiny.com>
2016-05-05 00:00:41 +0100

    Added Tile component

---
+++ Js/Webiny/Modules/Tile/Tile.jsx
@@ -0,0 +1,14 @@
+import Webiny from 'Webiny';
+
+class Tile extends Webiny.Ui.Component {
+
+}
+
+Tile.defaultProps = {
+	renderer() {
+		const classes = this.classSet('tile', this.props.className);
+		return <div className={classes}>{this.props.children}</div>;
+	}
+};
+
+export default Tile;
