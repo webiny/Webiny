ef641e748a6b2d3f0e61ae8b89e0ed5ce4a2b826
adrian.1358@gmail.com <4ndr055!>
2016-05-05 10:54:16 +0200

    mini fix on field

--- Js/Webiny/Modules/List/Components/Table/Field.jsx
+++ Js/Webiny/Modules/List/Components/Table/Field.jsx
@@ -2,33 +2,33 @@ import Webiny from 'Webiny';
 
 class Field extends Webiny.Ui.Component {
 
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.bindMethods('getTdClasses');
-    }
+		this.bindMethods('getTdClasses');
+	}
 
-    getTdClasses(classes = {}) {
-        const coreClasses = {};
-        coreClasses[this.props.sortedClass] = this.props.sorted !== null;
-        coreClasses[this.props.alignLeftClass] = this.props.align === 'left';
-        coreClasses[this.props.alignRightClass] = this.props.align === 'right';
-        coreClasses[this.props.alignCenterClass] = this.props.align === 'center';
-        return this.classSet(coreClasses, this.props.className, classes);
-    }
+	getTdClasses(classes = {}) {
+		const coreClasses = {};
+		coreClasses[this.props.sortedClass] = this.props.sorted !== null;
+		coreClasses[this.props.alignLeftClass] = this.props.align === 'left';
+		coreClasses[this.props.alignRightClass] = this.props.align === 'right';
+		coreClasses[this.props.alignCenterClass] = this.props.align === 'center';
+		return this.classSet(coreClasses, this.props.className, classes);
+	}
 }
 
 Field.defaultProps = {
-    default: '-',
-    sortedClass: 'sorted',
-    alignLeftClass: 'text-left',
-    alignRightClass: 'text-right',
-    alignCenterClass: 'text-center',
-    renderer() {
-        return (
-            <td className={this.getTdClasses()}>{_.get(this.props.data, this.props.name, this.props.default)}</td>
-        );
-    }
+	default: '-',
+	sortedClass: 'sorted',
+	alignLeftClass: 'text-left',
+	alignRightClass: 'text-right',
+	alignCenterClass: 'text-center',
+	renderer() {
+		return (
+			<td className={this.getTdClasses()}>{_.get(this.props.data, this.props.name) || this.props.default}</td>
+		);
+	}
 };
 
 export default Field;
