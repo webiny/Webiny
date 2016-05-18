4a04cabf95f17b802dd83e124ecb9d983b87016c
Pavel910 <pavel@webiny.com>
2016-05-18 18:54:38 +0200

    Custom action renderer via children function

--- Js/Webiny/Modules/List/Components/Table/Actions/Action.jsx
+++ Js/Webiny/Modules/List/Components/Table/Actions/Action.jsx
@@ -8,6 +8,10 @@ class Action extends Webiny.Ui.Component {
 Action.defaultProps = {
     onClick: _.noop,
     renderer() {
+        if (_.isFunction(this.props.children)) {
+            return this.props.children.call(this, this.props.data, this);
+        }
+
         return (
             <Ui.Link onClick={() => this.props.onClick(this.props.data)}>{this.props.label}</Ui.Link>
         );
