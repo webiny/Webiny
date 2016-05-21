8e340b9ff7508a505ab68cb4da38427c9088420f
adrian.1358@gmail.com <4ndr055!>
2016-05-21 12:45:44 +0200

    form container loader

--- Js/Webiny/Modules/Form/Module.js
+++ Js/Webiny/Modules/Form/Module.js
@@ -1,13 +1,14 @@
 import Webiny from 'Webiny';
 import Container from './FormContainer';
 import Error from './FormContainerError';
+import Loader from './FormContainerLoader';
 import Fieldset from './Fieldset';
 import DelayedValueLink from './DelayedValueLink';
 
 class Module extends Webiny.Module {
 
     init() {
-        Webiny.Ui.Components.Form = {Container, Fieldset, Error};
+        Webiny.Ui.Components.Form = {Container, Fieldset, Error, Loader};
         Webiny.Ui.Components.DelayedValueLink = DelayedValueLink;
     }
 }
