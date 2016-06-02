d75f7b8b25346e81e1595d6fbfa45aecbf50910f
adrian.1358@gmail.com <4ndr055!>
2016-06-02 10:38:45 +0200

    - simplified i18n text replacing and now supporting output consisting of html and jsx components

--- Js/Webiny/Modules/Core/i18n/i18n.js
+++ Js/Webiny/Modules/Core/i18n/i18n.js
@@ -1,34 +1,53 @@
-function replaceVariables(text, values) {
-    const re = /(\{.*?\})/g;
-    let m = re.exec(text);
-    const variables = [];
+const translations = {
+    en_GB: {
+        'Selecto.Backend.Billing.InvoicesList.c7699f35c47afc1dc52a7bb914be10b3': 'Are you sure you want to approve invoice {invoiceNumber}?'
+    }
+};
 
-    // Collect variables
-    while (m !== null) {
-        if (m.index === re.lastIndex) {
-            re.lastIndex++;
-        }
-        variables.push(_.trim(m[0], '{}'));
-        m = re.exec(text);
+/**
+ * This is responsible for replacing given text with given values
+ * It will automatically detect if it needs to return a string or JSX based on given variables
+ * (if all variables are strings, then final output will also be returned as string)
+ * @param text
+ * @param values
+ * @returns {*}
+ */
+function replaceVariables(text, values) {
+    if (_.isEmpty(values)) {
+        return text;
     }
 
-    // Replace variables
-    variables.forEach(variable => {
-        text = text.replace(`{${variable}}`, values[variable]);
+    // Let's first check if we need to return pure string or JSX
+    let stringOutput = true;
+    _.each(values, (value) => {
+        if (!_.isString(value)) {
+            stringOutput = false;
+        }
     });
 
-    return text;
+    const parts = text.split(/(\{.*?\})/);
+
+    if (stringOutput) {
+        let output = '';
+        parts.forEach(part => {
+            output += _.startsWith(part, '{') ? values[_.trim(part, '{}')] : part;
+        });
+        return output;
+    }
+
+    // Let's create a JSX output
+    return parts.map((part, index) => {
+        if (_.startsWith(part, '{')) {
+            return <webiny-i18n-part key={index}>{values[_.trim(part, '{}')]}</webiny-i18n-part>;
+        }
+        return <webiny-i18n-part key={index}>{part}</webiny-i18n-part>;
+    });
 }
 
-const i18n = function i18n(key, text, variables, options = {}) {
-    const translations = {
-        en_GB: {
-            'Selecto.Backend.Billing.InvoicesList.c7699f35c47afc1dc52a7bb914be10b3': 'Are you sure you want to approve invoice {invoiceNumber}?'
-        }
-    };
 
+const i18n = function i18n(key, text, variables) {
     const output = _.get(translations[i18n.language], key, text);
-    return replaceVariables(output, variables, options);
+    return replaceVariables(output, variables);
 };
 
 i18n.language = 'en_GB';
