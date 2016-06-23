4d23fd95cf914646b4b96e5fcb4cc5cc22d83ee1
Pavel910 <pavel@webiny.com>
2016-06-23 15:25:01 +0200

    Better app bootstrap control

--- Php/DevTools/BootstrapTrait.php
+++ Php/DevTools/BootstrapTrait.php
@@ -8,18 +8,33 @@
 namespace Apps\Core\Php\DevTools;
 
 use Apps\Core\Php\DevTools\Response\HtmlResponse;
+use Apps\Core\Php\PackageManager\App;
 
 /**
- * AppBootstrap class.
+ * BootstrapTrait
  *
- * This class contains a few
- *
+ * This trait contains a few helper methods to simplify app bootstrap
  */
 trait BootstrapTrait
 {
     use DevToolsTrait;
 
-    protected function addAppRoute($regex, $template, $priority = 400, $dataSource = null)
+    /**
+     * Run the app
+     *
+     * @param App $app Instance of PackageManager\App being run
+     */
+    abstract public function run(App $app);
+
+    /**
+     * Add an app route and a template that will be rendered for that route.<br/>
+     *
+     * @param string         $regex
+     * @param string         $template
+     * @param int            $priority
+     * @param array|callable $dataSource
+     */
+    protected function addAppRoute($regex, $template, $priority = 400, $dataSource = [])
     {
         $this->wEvents()->listen('Core.Bootstrap.Request', function () use ($regex, $template, $dataSource) {
             if ($this->wRequest()->getCurrentUrl(true)->getPath(true)->match($regex)) {
