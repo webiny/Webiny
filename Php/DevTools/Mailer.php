195e0ee99eee6135905b08a57b216cca9eae87c7
Pavel910 <pavel@webiny.com>
2016-08-12 12:21:21 +0200

    Added Mailer wrapper and wMailer() method to WebinyTrait

---
+++ Php/DevTools/Mailer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Webiny Platform (http://www.webiny.com/)
+ *
+ * @copyright Copyright Webiny LTD
+ */
+
+namespace Apps\Core\Php\DevTools;
+
+use Webiny\Component\Mailer\MailerTrait;
+use Webiny\Component\StdLib\SingletonTrait;
+
+/**
+ * Mailer class.
+ *
+ * This class is a wrapper for Mailer component
+ *
+ */
+class Mailer
+{
+    use SingletonTrait, MailerTrait;
+
+    public function getMailer($name = 'Default')
+    {
+        return $this->mailer($name);
+    }
+}
