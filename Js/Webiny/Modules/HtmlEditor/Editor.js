6dad6f56a996ac46942d1655499381306dc1f48d
adrian.1358@gmail.com <4ndr055!>
2016-08-14 14:28:55 +0200

    svg path can be changed through props

--- Js/Webiny/Modules/HtmlEditor/Editor.js
+++ Js/Webiny/Modules/HtmlEditor/Editor.js
@@ -13,7 +13,7 @@ class Editor extends Webiny.Ui.FormComponent {
     componentDidMount() {
         super.componentDidMount();
 
-        $.trumbowyg.svgPath = Webiny.Assets('Core.Backend', 'images/trumbowyg.svg');
+        $.trumbowyg.svgPath = this.props.svgPath;
 
         const options = {
             btnsDef: {
@@ -59,7 +59,7 @@ class Editor extends Webiny.Ui.FormComponent {
 }
 
 Editor.defaultProps = {
-
+    svgPath: Webiny.Assets('Core.Backend', 'images/trumbowyg.svg'),
     renderer() {
         return (
             <div>
