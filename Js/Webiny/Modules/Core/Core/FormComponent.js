e68e009ffa4a883a1b5c78f0b9c8e6c3d7c9ea33
Pavel910 <pavel@webiny.com>
2016-09-02 15:11:21 +0200

    Component.js now has isMounted() method for rare cases of checking if component is still mounted or not (when we are not sure if this.setState is safe to call). FormComponent now contains default props shared among all form components (still in progress).

--- Js/Webiny/Modules/Core/Core/FormComponent.js
+++ Js/Webiny/Modules/Core/Core/FormComponent.js
@@ -105,7 +105,11 @@ FormComponent.defaultProps = {
     disabledBy: null,
     form: null,
     validate: null,
-    valueLink: null
+    valueLink: null,
+    hideAnimation: {translateY: 0, opacity: 0, duration: 225},
+    showAnimation: {translateY: 50, opacity: 1, duration: 225},
+    showValidationMessage: true,
+    showValidationIcon: true
 };
 
 export default FormComponent;
