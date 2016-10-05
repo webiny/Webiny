de5438f9a344beddb90140fae68b6d0be5de528a
Pavel910 <pavel@webiny.com>
2016-10-05 11:40:36 +0200

    Added support for anchor links scrolling

--- Js/Webiny/Modules/Core/Router/Router.js
+++ Js/Webiny/Modules/Core/Router/Router.js
@@ -269,15 +269,23 @@ class Router {
     handleAnchorClick(a, e) {
         let url = a.href;
 
+        // _blank links should not be intercepted
         if (a.target === '_blank') {
             return;
         }
 
+        // Prevent scrolling to top when clicking on '#' link
         if (_.endsWith(url, '#')) {
             e.preventDefault();
             return;
         }
 
+        // Check if it's an anchor link
+        if (url.indexOf('#') > -1) {
+            return;
+        }
+
+        // Push state and let the Router process the rest
         if (url.indexOf(webinyWebPath) === 0) {
             e.preventDefault();
             url = url.replace(webinyWebPath, '');
