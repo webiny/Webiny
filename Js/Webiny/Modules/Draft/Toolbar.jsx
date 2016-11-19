fe8e3394af5ccf4358c9c39935fcf6b9942817eb
SvenAlHamad <sven@webiny.com>
2016-11-19 20:04:55 +0000

    Floating toolbar improvements.

--- Js/Webiny/Modules/Draft/Toolbar.jsx
+++ Js/Webiny/Modules/Draft/Toolbar.jsx
@@ -1,25 +1,40 @@
 import Webiny from 'Webiny';
 
 class Toolbar extends Webiny.Ui.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            'floatingToolbarInterval': null
+        };
+    }
+
     componentDidMount() {
         super.componentDidMount();
 
-        $(document).on('scroll', () => {
+        const floatingToolbarInterval = setInterval(()=> {
             const doc = $(document);
             const elem = $(ReactDOM.findDOMNode(this));
-            if (doc.scrollTop() > 490) {
+            if (doc.scrollTop() > (elem.closest('.rich-editor').offset().top - 49)) {
                 elem.addClass('floating');
-                elem.css({width: elem.closest('.rich-editor').width()});
+                elem.css({width: elem.closest('.rich-editor').outerWidth() - 2});
+                elem.css({left: elem.closest('.rich-editor').offset().left + 6});
+                elem.closest('.rich-editor').addClass('toolbar-floated');
             } else {
+                elem.closest('.rich-editor').removeClass('toolbar-floated');
                 elem.removeClass('floating');
                 elem.css({width: 'auto'});
+                elem.css({left: '0'});
             }
-        });
+        }, 150);
+
+        this.setState({'floatingToolbarInterval': floatingToolbarInterval});
     }
 
     componentWillUnmount() {
         super.componentWillUnmount();
-        $(document).off('scroll');
+        clearInterval(this.state.floatingToolbarInterval);
     }
 }
 
