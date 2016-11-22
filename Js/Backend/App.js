927b208ba0d7678aec521f15f0c92a5550792085
Pavel910 <pavel@webiny.com>
2016-11-22 18:19:14 +0100

    Added manual setter of xdebug parameter when xdebug_session cookie is defined, for situations when we have separate API domain.

--- Js/Backend/App.js
+++ Js/Backend/App.js
@@ -2,6 +2,12 @@ import Webiny from 'Webiny';
 
 const backend = new Webiny.App('Core.Backend');
 backend.beforeRender(() => {
+    Webiny.Http.addRequestInterceptor(http => {
+        if (Webiny.Cookies.get('XDEBUG_SESSION')) {
+            http.query.XDEBUG_SESSION_START = Webiny.Cookies.get('XDEBUG_SESSION');
+        }
+    });
+
     // Load other backend apps
     const api = new Webiny.Api.Endpoint('/services/core/apps');
     return api.get('/backend').then(res => {
