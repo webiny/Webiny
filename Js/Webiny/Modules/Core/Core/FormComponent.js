f3baba2b9117911f88e32fe4f4faaf04bc9569ac
Pavel910 <pavel@webiny.com>
2016-12-13 16:07:54 +0100

    FormComponent now uses Promise class instead of Q.

--- Js/Webiny/Modules/Core/Core/FormComponent.js
+++ Js/Webiny/Modules/Core/Core/FormComponent.js
@@ -64,7 +64,7 @@ class FormComponent extends Component {
                 return validationResult;
             });
         }
-        return Q(true);
+        return Promise.resolve(true);
     }
 
     isValid() {
