9cc7c533631a60081026cd0041fdac40c4cdb99e
Pavel910 <pavel@webiny.com>
2016-12-20 00:48:06 +0100

    Tabs component rewritten for better control and customization.

--- Js/Webiny/Modules/Tabs/Module.js
+++ Js/Webiny/Modules/Tabs/Module.js
@@ -1,11 +1,16 @@
 import Webiny from 'Webiny';
-import {Tabs, Tab} from './Tabs';
+import Tabs from './Tabs';
+import Tab from './Tab';
+import TabHeader from './TabHeader';
+import TabContent from './TabContent';
 
 class Module extends Webiny.Module {
 
     constructor(app) {
         super(app);
         Webiny.Ui.Components.Tabs = Tabs;
+        Tab.Header = TabHeader;
+        Tab.Content = TabContent;
         Tabs.Tab = Tab;
     }
 }
--- Js/Webiny/Modules/Tabs/Tabs.jsx
+++ Js/Webiny/Modules/Tabs/Tabs.jsx
@@ -1,74 +1,19 @@
 import Webiny from 'Webiny';
-const Ui = Webiny.Ui.Components;
+import Tab from './Tab';
 
-export class Tab extends Webiny.Ui.Component {
-
-}
-
-Tab.defaultProps = {
-    label: 'Tab',
-    alwaysRender: true,
-    disabled: false,
-    onClick: _.noop,
-    icon: null,
-    // The following props are passed from Tabs component
-    render: 'tab', // tab or content,
-    active: false,
-    index: null,
-    parent: null,
-    renderer() {
-        const onClick = (e) => {
-            this.props.onClick(this, this.props.index, e);
-            if (!e.isDefaultPrevented()) {
-                this.props.parent.selectTab(this.props.index);
-            }
-        };
-
-        const active = {active: this.props.active};
-        const tabSelectorClass = this.classSet(active, this.props.disabled ? 'disabled' : '');
-
-        const icon = this.props.icon ? <Ui.Icon icon={'left ' + this.props.icon}/> : null;
-        let label = this.props.label;
-        if (_.isString(this.props.label)) {
-            label = (
-                <a href="javascript:void(0);">
-                    {icon}
-                    {this.props.label}
-                </a>
-            );
-        }
-
-        if (this.props.render === 'tab') {
-            return (
-                <li className={tabSelectorClass} onClick={onClick}>{label}</li>
-            );
-        }
-
-        const tabClass = this.classSet('tab-pane', active);
-
-        if (!this.props.disabled && (this.props.active || this.props.alwaysRender)) {
-            return (
-                <div className={tabClass}>
-                    {this.props.children}
-                    <div className="clearfix"></div>
-                </div>
-            );
-        }
-
-        return null;
-    }
-};
-
-export class Tabs extends Webiny.Ui.Component {
+class Tabs extends Webiny.Ui.Component {
 
     constructor() {
         super();
-        this.tabs = [];
         this.state = {
             selected: 0
         };
 
-        this.bindMethods('selectTab');
+        this.tabs = [];
+        this.tabsHeader = [];
+        this.tabsContent = [];
+
+        this.bindMethods('parseChildren,selectTab,renderTabs,renderHeader,renderContent');
     }
 
     selectTab(index) {
@@ -98,35 +43,68 @@ export class Tabs extends Webiny.Ui.Component {
         super.componentWillReceiveProps(props);
         this.setState({selected: props.selected || this.state.selected});
     }
+
+    parseChildren(props, state) {
+        this.tabsHeader = [];
+        this.tabsContent = [];
+
+        React.Children.map(props.children, (child, index) => {
+            const active = parseInt(state.selected) === index;
+
+            const headerProps = {
+                key: index,
+                active
+            };
+
+            _.assign(headerProps, _.omit(child.props, ['renderer', 'children', 'tabContentRenderer']));
+
+            let tabClicked = (e) => {
+                // Pass instance of Tabs, index clicked and event.
+                child.props.onClick(this, index, e);
+                if (!e.isDefaultPrevented()) {
+                    this.selectTab(index);
+                }
+            };
+
+            this.tabsHeader.push(
+                <Tab.Header {...headerProps} onClick={tabClicked}/>
+            );
+
+            const contentProps = {
+                key: index,
+                active
+            };
+            _.assign(contentProps, _.omit(child.props, ['renderer', 'tabContentRenderer']));
+            if (_.has(child.props, 'tabContentRenderer')) {
+                contentProps.renderer = child.props.tabContentRenderer;
+            }
+            this.tabsContent.push(
+                <Tab.Content {...contentProps}/>
+            );
+        });
+    }
+
+    renderTabs() {
+        return this.props.tabsRenderer.call(this);
+    }
+
+    renderHeader() {
+        return this.props.headerRenderer.call(this);
+    }
+
+    renderContent() {
+        return this.props.contentRenderer.call(this);
+    }
 }
 
 Tabs.defaultProps = {
     position: 'top', // top, left
     size: 'default',
-    renderer() {
+    tabsRenderer() {
         const tabsContainerCss = this.classSet({
             'tabs': true,
             'tabs--navigation-top': this.props.position === 'top',
             'tabs--navigation-left': this.props.position === 'left'
-        });
-
-        const tabsHeader = [];
-        const tabsContent = [];
-
-        React.Children.map(this.props.children, (child, index) => {
-            const active = parseInt(this.state.selected) === index;
-
-            const props = {
-                key: index,
-                index,
-                active,
-                parent: this,
-                renderIf: _.get(child.props, 'renderIf', true)
-            };
-            props.render = 'tab';
-            tabsHeader.push(React.cloneElement(child, props, child.props.children));
-            props.render = 'content';
-            tabsContent.push(React.cloneElement(child, props, child.props.children));
         });
 
         const tabsNavClasses = {
@@ -137,10 +115,22 @@ Tabs.defaultProps = {
         return (
             <div className={tabsContainerCss}>
                 <div className="tabs__body">
-                    <ul className={this.classSet(tabsNavClasses)}>{tabsHeader}</ul>
-                    <div className="tabs__panes">{tabsContent}</div>
+                    <ul className={this.classSet(tabsNavClasses)}>{this.renderHeader()}</ul>
+                    <div className="tabs__panes">{this.renderContent()}</div>
                 </div>
             </div>
         );
+    },
+    headerRenderer() {
+        return this.tabsHeader;
+    },
+    contentRenderer() {
+        return this.tabsContent;
+    },
+    renderer() {
+        this.parseChildren(this.props, this.state);
+        return this.renderTabs();
     }
 };
+
+export default Tabs;
