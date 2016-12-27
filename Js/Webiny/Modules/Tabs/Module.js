9b9369f8aab645e99ed9dfc4258ab4eb9bcee82d
Pavel910 <pavel@webiny.com>
2016-12-27 15:49:20 +0100

    Tabs module structure

--- Js/Webiny/Modules/Tabs/Module.js
+++ Js/Webiny/Modules/Tabs/Module.js
@@ -9,8 +9,6 @@ class Module extends Webiny.Module {
     constructor(app) {
         super(app);
         Webiny.Ui.Components.Tabs = Tabs;
-        Tab.Header = TabHeader;
-        Tab.Content = TabContent;
         Tabs.Tab = Tab;
         Tab.Header = TabHeader;
         Tab.Content = TabContent;
