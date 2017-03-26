b8f46f45dd517dda939bf480838fc4642bd8b30e
sven <sven@webiny.com>
2017-03-26 13:57:22 +0100

    Modal header can how take child elements

--- Js/Webiny/Modules/Modal/Components/Header.js
+++ Js/Webiny/Modules/Modal/Components/Header.js
@@ -7,9 +7,17 @@ class Header extends Webiny.Ui.Component {
 Header.defaultProps = {
     onClose: _.noop,
     renderer() {
+
+        let headerContent = '';
+        if(_.get(this.props, 'title')){
+            headerContent = <h4>{this.props.title}</h4>;
+        }else if(this.props.children.length>0){
+            headerContent = this.props.children;
+        }
+
         return (
             <div className={this.classSet('modal-header', this.props.className)}>
-                <h4>{this.props.title}</h4>
+                {headerContent}
                 <button onClick={this.props.onClose} type="button" className="close md-close" data-dismiss="modal">×</button>
             </div>
         );
