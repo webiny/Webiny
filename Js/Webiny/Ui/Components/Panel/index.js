959068a44e470db1b1cc4b9836cb926820ea0e42
Pavel910 <pavel@webiny.com>
2017-04-20 21:32:19 +0200

    Added Panel component

---
+++ Js/Webiny/Ui/Components/Panel/index.js
@@ -0,0 +1,9 @@
+import Body from './Body';
+import Header from './Header';
+import Footer from './Footer';
+import Panel from './Panel';
+import Menu from './Menu';
+
+_.assign(Panel, {Header, Body, Footer, Menu});
+
+export default Panel;
