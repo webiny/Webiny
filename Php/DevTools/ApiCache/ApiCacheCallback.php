cc0cb38ceeb5fbf2566ae4d017e792a92df0c13b
SvenAlHamad <sven@webiny.com>
2017-06-09 21:07:08 +0100

    Improvements on ApiCache.

--- Php/DevTools/ApiCache/ApiCacheCallback.php
+++ Php/DevTools/ApiCache/ApiCacheCallback.php
@@ -6,7 +6,6 @@ use Apps\Webiny\Php\DevTools\Exceptions\AppException;
 use Apps\Webiny\Php\DevTools\WebinyTrait;
 use Webiny\Component\StdLib\StdLibTrait;
 use Webiny\Hrc\CacheRules\CacheRule;
-use Webiny\Hrc\Hrc;
 use Webiny\Hrc\ReadPayload;
 use Webiny\Hrc\SavePayload;
 
@@ -31,44 +30,20 @@ class ApiCacheCallback implements \Webiny\Hrc\EventCallbackInterface
 
         // if list, we need to extract entities
         $data = self::jsonDecode($payload->getContent(), true);
-        $list = $data['data'];
-        if (is_array($list) && isset($list['list'])) {
-            /**
-             * @var $hrc Hrc
-             */
-            $hrc = $this->wService('Hrc');
-            $cr = $payload->getRule()->getCacheRule();
 
-            // prepare the entity cache rule
-            $entityCacheRule = new CacheRule('_WebinyEntityCache', $cr->getTtl(), ['entityCache'], [], []);
-            $hrc->appendRule($entityCacheRule);
+        // if it's not an array we consider it an invalid response - do not cache
+        if (!$data || !is_array($data)) {
+            $payload->setSaveFlag(false);
 
-            $tags = [];
-            $cacheContent = [];
-            foreach ($list['list'] as $item) {
-                // compute item key
-                $key = $this->computeApiCacheKey($cr, $item);
+            return;
+        }
 
-                // store entity as separate cache entry
-                $key = $hrc->save('EntityCache' . $key, self::jsonEncode($item), '_WebinyEntityCache', [$item['id']]);
-                $entityCacheRule->setTags(['entityCache']);
-                $tags[] = $item['id'];
-
-                // store item
-                $cacheContent[] = $key;
-            }
-
-            // modify the cache content that should be stored
-            $data['data']['list'] = ['cacheList' => $cacheContent];
-            $payload->setContent(self::jsonEncode($data));
-            $payload->getRule()->getCacheRule()->appendTags($tags);
+        if (isset($data['data']['list'])) {
+            $this->cacheList($payload, $data);
+        } elseif (isset($data['data']['entity'])) {
+            $this->cacheEntity($payload, $data);
         } else {
-            // it's probably a single record
-            $data = self::jsonDecode($payload->getContent(), true);
-            if (isset($data['data']['id'])) {
-                $payload->setContent(self::jsonEncode($data['data']));
-                $payload->getRule()->getCacheRule()->appendTags(['entityCache', $data['data']['id']]);
-            }
+            // it's a custom response, in that case, no special processing is required
         }
 
     }
@@ -110,31 +85,24 @@ class ApiCacheCallback implements \Webiny\Hrc\EventCallbackInterface
     public function afterRead(ReadPayload $payload)
     {
         // check if cacheList
-        $data = self::jsonDecode($payload->getContent(), true);
-        if (is_array($data) && isset($data['data']['list']) && isset($data['data']['list']['cacheList'])) {
-            $list = $data['data']['list']['cacheList'];
+        $rawContent = $payload->getContent();
+        $data = self::jsonDecode($rawContent, true);
 
-            /**
-             * @var $hrc Hrc
-             */
-            $hrc = $this->wService('Hrc');
+        if (is_array($data) && isset($data['data']['list']) && isset($data['data']['meta']['cacheList'])) {
+            $list = $data['data']['list'];
 
             // extract cache list items and replace them with actual items
-            $items = [];
             foreach ($list as $cacheKey) {
-                $itemData = $hrc->readByCacheKey($cacheKey);
+                $itemData = self::wApiCache()->hrc()->readByCacheKey($cacheKey);
                 if (!$itemData) {
-                    $hrc->purgeByCacheKey($payload->getRule()->getCacheKey());
+                    self::wApiCache()->hrc()->purgeByCacheKey($payload->getRule()->getCacheKey());
                 } else {
-                    $items[] = self::jsonDecode($itemData);
+                    $itemData = self::jsonDecode($itemData, true);
+                    $rawContent = str_replace('"' . $cacheKey . '"', self::jsonEncode($itemData['data']['entity']), $rawContent);
                 }
             }
 
-            $data['data']['list'] = $items;
-            $payload->setContent(self::jsonEncode($data));
-        } else {
-            // single item entry
-            $payload->setContent(self::jsonEncode(['data' => $data]));
+            $payload->setContent($rawContent);
         }
     }
 
@@ -147,22 +115,28 @@ class ApiCacheCallback implements \Webiny\Hrc\EventCallbackInterface
      *
      * @param CacheRule $cr
      * @param array     $entity
+     * @param string    $fields List of fields as a string
      *
      * @return string
      * @throws AppException
      */
-    private function computeApiCacheKey(CacheRule $cr, $entity)
+    private function computeApiCacheKey(CacheRule $cr, $entity, $fields)
     {
+        // cache rule name is part of the key
+        $key = $cr->getName();
+
+        // list of fields is part of the cache key
+        $key .= $fields;
+
         // extract key items
-        if (isset($cr->getConfig()['Key'])) {
-            $keyItems = $cr->getConfig()['Key'];
+        if (isset($cr->getConfig()['Entity']['CacheIdFields'])) {
+            $keyItems = $cr->getConfig()['Entity']['CacheIdFields'];
             $keyItems[] = 'id';
         } else {
             $keyItems[] = 'id';
         }
 
         // loop through the items and concatenate the key
-        $key = '';
         foreach ($keyItems as $k) {
             if (!isset($entity[$k])) {
                 throw new AppException('Webiny cache: Unable to compute cache key for rule ' . $cr->getName() . ' because key ".' . $k . '." is missing.');
@@ -171,13 +145,89 @@ class ApiCacheCallback implements \Webiny\Hrc\EventCallbackInterface
             $key .= $entity[$k];
         }
 
-        // cache rule name is part of the key
-        $key .= $cr->getName();
-
-        // list of fields is part of the cache key
-        $key .= implode(',', array_keys($entity));
-
         // hash and return
         return md5($key);
+    }
+
+    /**
+     * When caching a list response, we extract the entities inside the list and cache them separately to the actual list.
+     *
+     * @param SavePayload $payload
+     * @param             $data
+     */
+    private function cacheList(SavePayload $payload, $data)
+    {
+        $cr = $payload->getRule()->getCacheRule();
+
+        // prepare the entity cache rule
+        $entityCacheRule = new CacheRule('_WebinyEntityCache', $cr->getTtl(), ['entityCache'], [], []);
+        self::wApiCache()->hrc()->appendRule($entityCacheRule);
+
+        // add the list cache key to meta, so we can share it across all linked entities
+        $data['data']['meta']['cacheKey'] = $payload->getRule()->getCacheKey();
+
+        // add cache rule to the key
+        $data['data']['meta']['cacheRule'] = $payload->getRule()->getCacheRule()->getName();
+
+        $tags = [];
+        $cacheContent = [];
+        foreach ($data['data']['list'] as $item) {
+            // compute item key
+            $key = $this->computeApiCacheKey($cr, $item, $data['data']['meta']['fields']);
+
+            // change the structure of the item so it matches an entity record - this way we can have an unified process when updating
+            // the cache records
+            $itemId = $item['id'];
+            $item = [
+                'data' => [
+                    'entity' => $item,
+                    'meta'   => $data['data']['meta']
+                ],
+            ];
+
+            // store entity as separate cache entry
+            $entityTags = [
+                $itemId,
+                $payload->getRule()->getCacheRule()->getName()
+            ];
+
+            $key = self::wApiCache()->hrc()->save('EntityCache' . $key, self::jsonEncode($item), '_WebinyEntityCache', $entityTags);
+            $entityCacheRule->setTags(['entityCache']);
+            $tags[] = $itemId;
+
+            // store item
+            $cacheContent[] = $key;
+        }
+
+        // modify the cache content that should be stored
+        $data['data']['meta']['cacheList'] = true;
+        $data['data']['list'] = $cacheContent;
+        $payload->setContent(self::jsonEncode($data));
+
+        // add cache key and cache name to tags
+        $tags[] = $payload->getRule()->getCacheRule()->getName();
+        $payload->getRule()->getCacheRule()->appendTags($tags);
+    }
+
+    /**
+     * When caching an entity, we just save it as it was returned by the api layer.
+     *
+     * @param SavePayload $payload
+     * @param             $data
+     */
+    private function cacheEntity(SavePayload $payload, $data)
+    {
+        // we can only cache an entity that has an id
+        if (isset($data['data']['entity']['id'])) {
+            $data['data']['meta']['cacheRule'] = $payload->getRule()->getCacheRule()->getName();
+            $data['data']['meta']['cacheKey'] = $payload->getRule()->getCacheKey();
+
+            $payload->setContent(self::jsonEncode($data));
+            $payload->getRule()->getCacheRule()->appendTags([
+                'entityCache',
+                $data['data']['entity']['id'],
+                $payload->getRule()->getCacheRule()->getName()
+            ]);
+        }
     }
 }
--- Php/DevTools/ApiCache/ApiCacheEntityTrait.php
+++ Php/DevTools/ApiCache/ApiCacheEntityTrait.php
@@ -2,9 +2,8 @@
 
 namespace Apps\Webiny\Php\DevTools\ApiCache;
 
-use Webiny\Component\Entity\EntityDataExtractor;
 use Webiny\Component\StdLib\StdLibTrait;
-use Webiny\Hrc\Hrc;
+use Webiny\Hrc\CacheRules\CacheRule;
 
 trait ApiCacheEntityTrait
 {
@@ -17,13 +16,35 @@ trait ApiCacheEntityTrait
     {
 
         $this->onAfterUpdate(function () {
-            $this->apiCacheUpdateRecord();
+            $this->apiCacheUpdateAction();
         });
 
 
         $this->onAfterDelete(function () {
-            $this->apiCacheDeleteRecord();
+            $this->apiCachePurgeRecord();
         });
+    }
+
+    /**
+     * Unlike the purge on update, which only purges the record inside the given cache rule, this method does a purge across all cache rules.
+     */
+    public function apiCachePurgeRecord()
+    {
+        // we need to get the cache rule from the item we want to purge, so that in case of a list, we also purge the items linked to the same list
+        $entries = self::wApiCache()->hrc()->readByTags(['entityCache', $this->id]);
+        if (!$entries) {
+            return false;
+        }
+
+        foreach ($entries as $cacheKey => $entry) {
+            $content = self::jsonDecode($entry['content'], true);
+
+            // purge linked items
+            self::wApiCache()->hrc()->purgeByTag($content['meta']['cacheKey']);
+        }
+
+        // purge everything else
+        self::wApiCache()->hrc()->purgeByTag($this->id);
     }
 
     /**
@@ -31,72 +52,117 @@ trait ApiCacheEntityTrait
      * with the new values. Note: ttl is not refreshed, as that would cause that in some cases a single list entry record expires
      * before the other linked records.
      */
-    private function apiCacheUpdateRecord()
+    private function apiCacheUpdateAction()
     {
-        /**
-         * @var $hrc Hrc
-         */
-        $hrc = $this->wService('Hrc');
-
         // get the list cache objects for the current entity
-        $entries = $hrc->readByTags(['entityCache', $this->id]);
+        $entries = self::wApiCache()->hrc()->readByTags(['entityCache', $this->id]);
+        if (!$entries) {
+            return false;
+        }
 
-        if ($entries) {
-            // update the data inside each of the given entries
-            foreach ($entries as $cacheKey => $entry) {
-                // update values on all levels
-                $entry['content'] = self::jsonDecode($entry['content'], true);
+        foreach ($entries as $cacheKey => $entry) {
+            // open up the entry content (content contains the thing we cached)
+            $entry['content'] = self::jsonDecode($entry['content'], true);
 
-                $updatedEntry = $entry['content'];
-                // we need to to toArray on every cache entry as we need to explicitly define which fields to return
-                foreach ($entry['content'] as $k => $v) {
-                    if (($val = $this->getAttribute($k)->getValue())) {
-                        $dataExtractor = new EntityDataExtractor($this);
-                        //$dataExtractor->buildEntityFields()
-                        $updatedEntry[$k] = $val;
+            // get fields for the current entry
+            $fields = $this->apiCacheExtractFields($entry);
+
+            // get the cache rule configuration
+            $cr = $this->apiCacheGetCacheRule($entry);
+
+            // check if the rule was deleted from the yaml config
+            if (!$cr) {
+                continue; // the ttl should naturally expire
+            }
+
+            // check the update action for that rule
+            $action = $this->apiCacheGetUpdateAction($cr);
+
+
+            if ($action == 'update') {
+                // check if skip fields are defined in the config
+                $skipFields = $this->apiCacheGetSkipFields($cr);
+
+                // get the new updated record with the required fields
+                $updatedRecord = $this->toArray($fields);
+
+                // we need to loop through the cache entry as we need to explicitly define which fields to skip and which to update
+                $updatedEntry = $entry['content']['data']['entity'];
+                foreach ($entry['content']['data']['entity'] as $k => $v) {
+                    // skip fields
+                    if (in_array($k, $skipFields)) {
+                        continue;
+                    }
+
+                    // we update only records that are available in the updatedRecord
+                    if (isset($updatedRecord[$k])) {
+                        $updatedEntry[$k] = $updatedRecord[$k];
                     } else {
+                        // if a record is no longer available, we remove it
                         unset($updatedEntry[$k]);
                     }
                 }
 
+                $entry['content']['data']['entity'] = $updatedEntry;
+
                 // re-store the updated entry into cache
-                $hrc->updateByCacheKey($cacheKey, self::jsonEncode($updatedEntry), $entry['ttl']);
-            }
-        }
-    }
+                // note: we can't change the ttl, as this might cause that some records inside a list expire later than others - would cause problems
+                self::wApiCache()->hrc()->updateByCacheKey($cacheKey, self::jsonEncode($entry['content']), $entry['ttl']);
+            } else {
+                // if we don't update, then we purge
 
-    private function apiCacheGetFieldList($cachedEntry, $MAXDEPTH = INF, $depth = 0, $arrayKeys = [])
-    {
-        if ($depth < $MAXDEPTH) {
-            $depth++;
-            $keys = array_keys($cachedEntry);
-            foreach ($keys as $key) {
-                if (is_array($cachedEntry[$key])) {
-                    $subKeys = $this->apiCacheGetFieldList($cachedEntry[$key], $MAXDEPTH, $depth);
-                    if (count($subKeys) > 0) {
-                        $arrayKeys[] = $key . '[' . implode(',', $subKeys) . ']';
-                    } else {
-                        $arrayKeys[] = $key;
-                    }
-                } else {
-                    $arrayKeys[] = $key;
-                }
+                // this purge clears all the lists that contain this entity id inside the current rule
+                self::wApiCache()->hrc()->purgeByTag([$this->id, $cr->getName()]);
             }
         }
 
-        return $arrayKeys;
     }
 
     /**
-     * When a record is deleted, we purge all cache entries that have this record id linked
+     * Check which fields need to be returned.
+     *
+     * @param $entry
+     *
+     * @return string
      */
-    private function apiCacheDeleteRecord()
+    private function apiCacheExtractFields($entry)
     {
-        /**
-         * @var $hrc Hrc
-         */
-        $hrc = $this->wService('Hrc');
-        $hrc->purgeByTag($this->id);
+        return isset($entry['content']['data']['meta']['fields']) ? $entry['content']['data']['meta']['fields'] : '*';
     }
 
+    /**
+     * @param $entry
+     *
+     * @return bool|\Webiny\Hrc\CacheRules\CacheRule
+     */
+    private function apiCacheGetCacheRule($entry)
+    {
+        return self::wApiCache()->hrc()->getCacheRule($entry['content']['data']['meta']['cacheRule']);
+    }
+
+    /**
+     * Check which purge action is defined inside the entity update action.
+     * If none is set, the default is purge.
+     *
+     * @param CacheRule $cr
+     *
+     * @return string
+     */
+    private function apiCacheGetUpdateAction(CacheRule $cr)
+    {
+        return isset($cr->getConfig()['Entity']['Update']['Action']) ? $cr->getConfig()['Entity']['Update']['Action'] : 'purge';
+    }
+
+    /**
+     * Checks if any fields shouldn't be updated on the update action.
+     * This is often used on dynamic fields and fields that are populated based on user permissions.
+     *
+     * @param CacheRule $cr
+     *
+     * @return array
+     */
+    private function apiCacheGetSkipFields(CacheRule $cr)
+    {
+        return isset($cr->getConfig()['Entity']['Update']['SkipFields']) ? $cr->getConfig()['Entity']['Update']['SkipFields'] : [];
+    }
 }
