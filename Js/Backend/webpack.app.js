4028d86852b050137b9cb23e89edbc7f60895e6d
Pavel910 <pavel@webiny.com>
2017-07-03 14:10:57 +0200

    Updated dependencies

--- Js/Backend/webpack.app.js
+++ Js/Backend/webpack.app.js
@@ -2,14 +2,14 @@ const Webiny = require('webiny/lib/webiny');
 const ChunkBundlerPlugin = require(Webiny.projectRoot('Apps/Webiny/Cli/Build/webpack/plugins/ChunkBundlerPlugin'));
 
 module.exports = (config) => {
-    const bundle = new ChunkBundlerPlugin({
+    /*const bundle = new ChunkBundlerPlugin({
         '/admin/acl/users': [
             'Webiny/Backend/Components/UserRoles',
             'Webiny/Backend/Components/UserPermissions'
         ]
     });
 
-    config.plugins.push(bundle);
+    config.plugins.push(bundle);*/
 
     return config;
 };
