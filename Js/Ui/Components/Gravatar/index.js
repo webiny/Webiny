4c02d4f6886951225a26acc6c7bfb965e28822d8
SvenAlHamad <sven@webiny.com>
2017-08-06 18:17:02 +0100

    Gravatar now supports className prop

--- Js/Ui/Components/Gravatar/index.js
+++ Js/Ui/Components/Gravatar/index.js
@@ -8,11 +8,13 @@ class Gravatar extends Webiny.Ui.Component {
 Gravatar.defaultProps = {
     hash: null,
     size: 48,
+    className: null,
     renderer() {
         const props = {
             src: '//www.gravatar.com/avatar/' + this.props.hash + '?s=' + this.props.size,
             width: this.props.size,
-            height: this.props.size
+            height: this.props.size,
+            className: this.props.className
         };
 
         return (
