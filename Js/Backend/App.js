69b95b04714e2e9ba9ec59de07096720e7389b7e
Pavel910 <pavel@webiny.com>
2017-08-11 12:49:11 +0200

    Moved app setup into App.js (removed configuration block from TPL).

--- Js/Backend/App.js
+++ Js/Backend/App.js
@@ -1,5 +1,7 @@
-import _ from 'lodash';
 import Webiny from 'webiny';
+import _ from 'lodash';
+import dynamics from 'dynamics.js';
+
 import Acl from './Modules/Acl';
 import Layout from './Modules/Layout';
 import Logger from './Modules/Logger';
@@ -15,24 +17,52 @@ class Backend extends Webiny.App {
             new Logger(this),
             new Dashboard(this)
         ];
+    }
 
-        this.beforeRender(() => {
+    run() {
+        this.loader = document.querySelector('.preloader-wrap');
+        return super.run().then(() => {
+            this.off = Webiny.Router.onRouteChanged(() => this.hideLoader());
+            // Configure Router
+            Webiny.Router.setBaseUrl(Webiny.Config.Js.Backend);
+            Webiny.Router.setTitlePattern('%s | Webiny');
+
             // Load other backend apps
             const api = new Webiny.Api.Endpoint('/services/webiny/apps');
             return api.get('/backend').then(res => {
                 let apps = Promise.resolve();
                 _.forIn(res.getData(), config => {
-                    apps = apps.then(() => Webiny.includeApp(config.name, config).then(app => {
-                        if (config.name === Webiny.Config.auth) {
-                            Webiny.Auth = app.getAuth();
-                        }
+                    apps = apps.then(() => Webiny.includeApp(config).then(app => {
                         app.run();
                     }));
                 });
                 return apps;
+            }).then(() => {
+                if (!Webiny.Auth) {
+                    Webiny.Auth = Webiny.Apps['Webiny.Skeleton'].getAuth();
+                }
             });
         });
     }
+
+    hideLoader() {
+        if (this.loader) {
+            this.off();
+            setTimeout(() => {
+                dynamics.animate(this.loader, {
+                    opacity: 0
+                }, {
+                    type: dynamics.easeOut,
+                    duration: 500,
+                    complete: () => {
+                        this.loader.style.display = 'none';
+                        this.loader = null;
+                    }
+                });
+            }, 100);
+        }
+    }
+
 }
 
 Webiny.registerApp(new Backend());
--- Js/Backend/Modules/Layout/index.js
+++ Js/Backend/Modules/Layout/index.js
@@ -6,11 +6,6 @@ class Layout extends Webiny.App.Module {
     init() {
         this.name = 'Layout';
         this.registerDefaultComponents({Footer});
-
-        // Remove route registered by Skeleton app
-        Webiny.Router.deleteRoute('Dashboard');
-        // Set a new default route
-        Webiny.Router.setDefaultRoute('Users.List');
     }
 }
 
