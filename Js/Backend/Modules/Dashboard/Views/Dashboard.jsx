e64543f6345a6562dd64796c67bf9495526f56bc
SvenAlHamad <sven@webiny.com>
2017-08-26 13:21:47 +0100

    Refactored dashboard to user the new dashboard view component.

--- Js/Backend/Modules/Dashboard/Views/Dashboard.jsx
+++ Js/Backend/Modules/Dashboard/Views/Dashboard.jsx
@@ -35,15 +35,12 @@ class Dashboard extends Webiny.Ui.View {
 Dashboard.defaultProps = {
     renderer() {
 
-        const {Gravatar, Button, Link, Icon} = this.props;
+        const {View, Gravatar, Button, Grid, Icon, Link} = this.props;
 
         return (
-            <div className="master-content master-dashboard--lite">
-                <div className="master-content__header">
-                    <div className="master-content__left">
-                        <h2 className="master-content__title">Dashboard</h2>
-                    </div>
-                    <div className="master-content__center">
+            <View.Dashboard>
+                <View.Header title="Dashboard">
+                    <View.Header.Center>
                         <div className="user-welcome">
                             <div className="user-welcome__avatar">
                                 <div className="avatar avatar--inline avatar--small">
@@ -54,70 +51,53 @@ Dashboard.defaultProps = {
                             </div>
                             <h3 className="user-welcome__message">Hi {this.getUserName()}</h3>
                         </div>
-                    </div>
-                    <div className="master-content__right">
-                        <Button onClick={() => Webiny.Router.goToRoute('Users.Account')}>Manage Account</Button>
-                    </div>
-                </div>
+                    </View.Header.Center>
+                    <Button onClick={() => Webiny.Router.goToRoute('Users.Account')}>Manage Account</Button>
+                </View.Header>
 
-                <div className="dashboard-content dashboard-content--lite">
-
+                <View.Body>
                     <Updates/>
+                    
+                    <Grid.Row>
+                        <Grid.Col all={4}>
+                            <View.InfoBlock title="GET STARTED">
+                                <ul>
+                                    <li>
+                                        <div className="block-list__item-image">
+                                            <Icon icon="icon-keys" size="2x"/>
+                                        </div>
+                                        <a href="https://www.webiny.com/docs/current/components/must-know"
+                                           className="block-list__item-text" target="_blank">
+                                            <strong>React Components</strong> - Learn what they do and how to implement them.
 
-                    <div className="row">
-                        <div className="col-md-4">
-                            <div className="block">
-                                <div className="block-header block-header-outer">
-                                    <h4 className="block-title">GET STARTED</h4>
-                                    <div className="header-options">
-                                    </div>
-                                </div>
-                                <div className="block-content">
-                                    <div>
-                                        <ul className="block-list">
-                                            <li className="block-list__item">
-                                                <div className="block-list__item-image">
-                                                    <Icon icon="icon-keys" size="2x"/>
-                                                </div>
-                                                <a href="https://www.webiny.com/docs/current/components/must-know"
-                                                   className="block-list__item-text" target="_blank">
-                                                    <strong>React Components</strong> - Learn what they do and how to implement them.
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <div className="block-list__item-image">
+                                            <Icon icon="fa-book " size="2x"/>
+                                        </div>
+                                        <a href="https://www.webiny.com/the-hub/tutorials" className="block-list__item-text"
+                                           target="_blank">
+                                            <strong>Tutorials</strong> - How to setup Webiny and other applications.
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <div className="block-list__item-image">
+                                            <Icon icon="fa-graduation-cap" size="2x"/>
+                                        </div>
+                                        <a href="https://www.webiny.com/docs/current/reference-manual/environments"
+                                           className="block-list__item-text" target="_blank">
+                                            <strong>Reference Manual</strong> - The nitty-gritty details of how the internal
+                                            components work.
+                                        </a>
+                                    </li>
+                                </ul>
+                            </View.InfoBlock>
+                        </Grid.Col>
 
-                                                </a>
-                                            </li>
-                                            <li className="block-list__item">
-                                                <div className="block-list__item-image">
-                                                    <Icon icon="fa-book " size="2x"/>
-                                                </div>
-                                                <a href="https://www.webiny.com/the-hub/tutorials" className="block-list__item-text"
-                                                   target="_blank">
-                                                    <strong>Tutorials</strong> - How to setup Webiny and other applications.
-                                                </a>
-                                            </li>
-                                            <li className="block-list__item">
-                                                <div className="block-list__item-image">
-                                                    <Icon icon="fa-graduation-cap" size="2x"/>
-                                                </div>
-                                                <a href="https://www.webiny.com/docs/current/reference-manual/environments"
-                                                   className="block-list__item-text" target="_blank">
-                                                    <strong>Reference Manual</strong> - The nitty-gritty details of how the internal
-                                                    components work.
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-md-4">
-                            <div className="block block--the-hub">
-                                <div className="block-header block-header-outer">
-                                    <div className="header-options">
-                                    </div>
-                                </div>
-                                <div className="block-content text-center">
-
+                        <Grid.Col all={4}>
+                            <View.InfoBlock title="THE HUB">
+                                <div className="text-center dashboard--block--the-hub">
                                     <div className="title-icon">
                                         <img src={imgInfinify} alt="Webiny Infinity"/>
                                     </div>
@@ -131,68 +111,63 @@ Dashboard.defaultProps = {
                                         <Link url="https://www.webiny.com/the-hub" newTab={true} type="primary">JOIN</Link>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
+                            </View.InfoBlock>
+                        </Grid.Col>
 
-                        <div className="col-md-4">
-                            <div className="block">
-                                <div className="block-header block-header-outer">
-                                    <h4 className="block-title">SOCIALIZE</h4>
-                                    <div className="header-options">
-                                    </div>
+                        <Grid.Col all={4}>
+                            <View.InfoBlock title="SOCIALIZE">
+                                <div>
+                                    <ul>
+                                        <li>
+                                            <div className="block-list__item-image">
+                                                <span className="icon icon-github icon-3x"></span>
+                                            </div>
+                                            <a href="https://github.com/Webiny" className="block-list__item-text" target="_blank">
+                                                <Icon icon="fa-github"/> GitHub
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <div className="block-list__item-image">
+                                                <span className="icon icon-twitter icon-3x"></span>
+                                            </div>
+                                            <a href="" className="block-list__item-text" target="_blank">
+                                                <Icon icon="fa-twitter"/> Twitter
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <div className="block-list__item-image">
+                                                <span className="icon icon-medium icon-3x"></span>
+                                            </div>
+                                            <a href="https://www.webiny.com/docs/current/components/must-know"
+                                               className="block-list__item-text" target="_blank">
+                                                <Icon icon="fa-medium"/> Blog
+                                            </a>
+                                        </li>
+
+                                        <li>
+                                            <div className="block-list__item-image">
+                                                <span className="icon icon-youtube icon-3x"></span>
+                                            </div>
+                                            <a href="https://www.webiny.com/docs/current/components/must-know"
+                                               className="block-list__item-text" target="_blank">
+                                                <Icon icon="fa-youtube"/> YouTube
+                                            </a>
+                                        </li>
+
+                                    </ul>
                                 </div>
-                                <div className="block-content block--the-hub">
-                                    <div>
-                                        <ul className="block-list">
-                                            <li className="block-list__item">
-                                                <div className="block-list__item-image">
-                                                    <span className="icon icon-github icon-3x"></span>
-                                                </div>
-                                                <a href="https://github.com/Webiny" className="block-list__item-text" target="_blank">
-                                                    <Icon icon="fa-github"/> GitHub
-                                                </a>
-                                            </li>
-                                            <li className="block-list__item">
-                                                <div className="block-list__item-image">
-                                                    <span className="icon icon-twitter icon-3x"></span>
-                                                </div>
-                                                <a href="" className="block-list__item-text" target="_blank">
-                                                    <Icon icon="fa-twitter"/> Twitter
-                                                </a>
-                                            </li>
-                                            <li className="block-list__item">
-                                                <div className="block-list__item-image">
-                                                    <span className="icon icon-medium icon-3x"></span>
-                                                </div>
-                                                <a href="https://www.webiny.com/docs/current/components/must-know"
-                                                   className="block-list__item-text" target="_blank">
-                                                    <Icon icon="fa-medium"/> Blog
-                                                </a>
-                                            </li>
+                            </View.InfoBlock>
+                        </Grid.Col>
 
-                                            <li className="block-list__item">
-                                                <div className="block-list__item-image">
-                                                    <span className="icon icon-youtube icon-3x"></span>
-                                                </div>
-                                                <a href="https://www.webiny.com/docs/current/components/must-know"
-                                                   className="block-list__item-text" target="_blank">
-                                                    <Icon icon="fa-youtube"/> YouTube
-                                                </a>
-                                            </li>
+                    </Grid.Row>
 
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                </View.Body>
 
-            </div>
+            </View.Dashboard>
         );
     }
 };
 
 export default Webiny.createComponent(Dashboard, {
-    modules: ['Gravatar', 'Button', 'Link', 'Icon']
+    modules: ['View', 'Gravatar', 'Button', 'Grid', 'Icon', 'Link']
 });
