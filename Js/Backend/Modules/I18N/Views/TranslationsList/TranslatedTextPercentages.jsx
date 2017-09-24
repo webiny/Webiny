a00465abc30506d62248423120af00d33961e8f0
adrian.1358@gmail.com <4ndr055!>
2017-09-24 12:17:07 +0200

    Progress bar tweak.

--- Js/Backend/Modules/I18N/Views/TranslationsList/TranslatedTextPercentages.jsx
+++ Js/Backend/Modules/I18N/Views/TranslationsList/TranslatedTextPercentages.jsx
@@ -6,6 +6,21 @@ import css from './TranslatedTextPercentages.scss';
  * @i18n.namespace  Webiny.Backend.I18N.TranslatedTextPercentages
  */
 class TranslatedTextPercentages extends Webiny.Ui.Component {
+    renderProgressBar(locale, data) {
+        let label = 'N/A';
+        let percentage = 0;
+        if (data.texts.total) {
+            percentage = (locale.count / data.texts.total * 100).toFixed(0);
+            label = `${locale.count} / ${data.texts.total} (${percentage}%)`;
+        }
+
+        return (
+            <progress-bar>
+                <bar style={{width: percentage + '%'}}/>
+                <label>{label}</label>
+            </progress-bar>
+        );
+    }
 }
 
 TranslatedTextPercentages.defaultProps = {
@@ -17,24 +32,18 @@ TranslatedTextPercentages.defaultProps = {
                 <Ui.Data api="/entities/webiny/i18n-texts/stats/translated">
                     {data => (
                         <Ui.Grid.Row>
-                            {data.translations.map(item => {
-                                const percentage = (item.count / data.texts.total * 100).toFixed(0);
-                                return (
-                                    <Ui.Grid.Col
-                                        key={item.locale.key}
-                                        className={css.translatedTextPercentagesLocaleStats}
-                                        xs="12"
-                                        sm="6"
-                                        md="4"
-                                        lg="3">
-                                        <strong>{item.locale.label}</strong>
-                                        <progress-bar>
-                                            <bar style={{width: percentage + '%'}}/>
-                                            <label>{item.count} / {data.texts.total} ({percentage}%)</label>
-                                        </progress-bar>
-                                    </Ui.Grid.Col>
-                                );
-                            })}
+                            {data.translations.map(item => (
+                                <Ui.Grid.Col
+                                    key={item.locale.key}
+                                    className={css.translatedTextPercentagesLocaleStats}
+                                    xs="12"
+                                    sm="6"
+                                    md="4"
+                                    lg="3">
+                                    <strong>{item.locale.label}</strong>
+                                    {this.renderProgressBar(item, data)}
+                                </Ui.Grid.Col>
+                            ))}
                         </Ui.Grid.Row>
                     )}
                 </Ui.Data>
