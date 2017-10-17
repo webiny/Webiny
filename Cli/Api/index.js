04f1b00385d37b90563d414c4f3da4f1a65460e6
Pavel910 <pavel@webiny.com>
2017-10-17 14:46:09 +0200

    Api plugin updated with `status` URL to retrieve state of webiny-cli

--- Cli/Api/index.js
+++ Cli/Api/index.js
@@ -1,23 +1,54 @@
 const Webiny = require('webiny-cli/lib/webiny');
 const Plugin = require('webiny-cli/lib/plugin');
+const _ = require('lodash');
 
 class Api extends Plugin {
-    constructor(program) {
-        super(program);
+    constructor() {
+        super();
+        this.currentTask = [];
+        this.taskLog = [];
 
-        this.selectApps = false;
+        Webiny.on('beforeTask', ({task}) => {
+            this.currentTask.push(task);
+            this.taskLog.push({status: 'started', task, ts: Date.now()});
+        });
 
+        Webiny.on('afterTask', ({task, data, err}) => {
+            this.currentTask.splice(this.currentTask.indexOf(task), 1);
+            const ts = Date.now();
+            const log = _.findLastIndex(this.taskLog, l => l.task === task);
+            const duration = (ts - this.taskLog[log].ts) / 1000;
+
+            this.taskLog.push({status: 'finished', ts, duration, task, data, err});
+        });
+
+        this.runHttpServer();
+    }
+
+    runHttpServer() {
+        const wConfig = Webiny.getConfig();
         const _ = require('lodash');
         const express = require('express');
-        const port = _.get(Webiny.getConfig(), 'cli.port', 3000);
+        const port = _.get(wConfig, 'cli.port', 3000);
 
         const app = express();
+        app.get('/status', (req, res) => {
+            res.end(JSON.stringify({
+                uptime: (Date.now() - this.apiStarted) / 1000,
+                env: wConfig.env,
+                cwd: Webiny.projectRoot(),
+                state: this.currentTask.length ? 'working' : 'idle',
+                task: this.currentTask,
+                log: this.taskLog
+            }));
+        });
+
         app.use((req, res, next) => {
             const url = require('url').parse(req.url, true);
             const data = url.query;
 
             // Make sure apps is an array
-            if(data.apps && _.isString(data.apps)) {
+            if (data.apps && _.isString(data.apps)) {
                 data.apps = [data.apps];
             }
 
@@ -36,6 +67,7 @@ class Api extends Plugin {
         });
 
         const server = app.listen(port, (err) => {
+            this.apiStarted = Date.now();
             if (err) {
                 console.log(err);
             }
