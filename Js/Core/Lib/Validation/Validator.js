65b0316254f3a45d0260a2eff781056f5afd55a2
Pavel910 <pavel@webiny.com>
2017-10-23 13:41:30 +0200

    [Fix] `required` validator is no longer triggered for 0 value.

--- Js/Core/Lib/Validation/Validator.js
+++ Js/Core/Lib/Validation/Validator.js
@@ -138,7 +138,7 @@ class Validator {
 const formValidator = new Validator();
 
 formValidator.addValidator('required', (value) => {
-    if (!(!value || value === '' || value === 0 || (_.isArray(value) && value.length === 0))) {
+    if (!(!value || value === '' || (_.isArray(value) && value.length === 0))) {
         return true;
     }
     throw new ValidationError(Webiny.I18n('This field is required'));
