e89fcaad6b99dbeddea97741a4826600b8d27b36
Adrian Smijulj <adrian1358@gmail.com>
2018-11-08 08:41:49 +0100

    feat(RouteAction): add "separate" and "url" props.

--- Js/Ui/Components/List/Components/Table/Actions/RouteAction.jsx
+++ Js/Ui/Components/List/Components/Table/Actions/RouteAction.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import _ from 'lodash';
-import Webiny from 'webiny';
+import React from "react";
+import _ from "lodash";
+import Webiny from "webiny";
 
 class RouteAction extends Webiny.Ui.Component {
     constructor(props) {
         super(props);
 
-        this.bindMethods('getRoute,getParams');
+        this.bindMethods("getRoute,getParams");
     }
 
     getRoute() {
-        return _.isFunction(this.props.route) ? this.props.route(this.props.data) : this.props.route;
+        return _.isFunction(this.props.route)
+            ? this.props.route(this.props.data)
+            : this.props.route;
+    }
+
+    getUrl() {
+        return _.isFunction(this.props.url) ? this.props.url(this.props.data) : this.props.url;
     }
 
     getParams() {
-        let params = !this.props.params ? Webiny.Router.getRoute(this.getRoute()).paramNames : _.clone(this.props.params);
+        let params = !this.props.params
+            ? Webiny.Router.getRoute(this.getRoute()).paramNames
+            : _.clone(this.props.params);
 
         if (_.isString(params) || _.isArray(params)) {
-            const paramNames = _.isString(params) ? params.split(',') : params;
+            const paramNames = _.isString(params) ? params.split(",") : params;
             params = {};
             paramNames.map(p => {
                 params[p] = this.props.data[p];
@@ -38,20 +46,34 @@ RouteAction.defaultProps = {
     data: {},
     label: null,
     hide: null,
+    separate: false,
     renderer() {
         if (_.isFunction(this.props.hide) && this.props.hide(this.props.data)) {
             return null;
         }
 
-        const {Link, Icon} = this.props;
+        const { Link, Icon, separate } = this.props;
+
+        const props = {
+            separate,
+            params: this.getParams()
+        };
+
+        if (this.props.route) {
+            props.route = this.getRoute();
+        }
+
+        if (this.props.url) {
+            props.url = this.getUrl();
+        }
 
         return (
-            <Link route={this.getRoute()} params={this.getParams()}>
-                {this.props.icon ? <Icon icon={this.props.icon}/> : null}
+            <Link {...props}>
+                {this.props.icon ? <Icon icon={this.props.icon} /> : null}
                 {this.props.label || this.props.children}
             </Link>
         );
     }
 };
 
-export default Webiny.createComponent(RouteAction, {modules: ['Link', 'Icon']});
+export default Webiny.createComponent(RouteAction, { modules: ["Link", "Icon"] });
